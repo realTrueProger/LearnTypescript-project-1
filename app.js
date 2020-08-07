8ff17775f1e0a7078988cc275d7084c718ceca8b
Vladimir Solovev <vovasollo@yandex.ru>
2020-08-07 17:32:06 +0300

    add inputForm submit handler and autobind decorator

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Autobind(target, methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this);
+        }
+    };
+    return adjustedDescriptor;
+}
 class ProjectInputForm {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.appRootElement = document.getElementById('app');
+        this.formElement = this.getFormFromTemplate();
+        this.titleInputElement = this.formElement.querySelector('#title');
+        this.descriptionInputElement = this.formElement.querySelector('#description');
+        this.peopleInputElement = this.formElement.querySelector('#people');
+        this.formElement.addEventListener('submit', this.submitHandler);
         this.renderHtml();
     }
-    renderHtml() {
+    submitHandler(e) {
+        e.preventDefault();
+        console.log('submit');
+        console.log(this.titleInputElement.value);
+    }
+    getFormFromTemplate() {
         const templateContent = document.importNode(this.templateElement.content, true);
-        const templateHtmlElement = templateContent.firstElementChild;
-        this.appRootElement.insertAdjacentElement('afterbegin', templateHtmlElement);
+        return templateContent.firstElementChild;
+    }
+    renderHtml() {
+        this.appRootElement.insertAdjacentElement('afterbegin', this.formElement);
     }
 }
+__decorate([
+    Autobind
+], ProjectInputForm.prototype, "submitHandler", null);
 const projectInput = new ProjectInputForm();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,18 +1,51 @@
+function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this);
+        }
+    };
+
+    return adjustedDescriptor;
+}
+
 class ProjectInputForm {
     templateElement: HTMLTemplateElement;
     appRootElement: HTMLDivElement;
+    formElement: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.appRootElement = document.getElementById('app')! as HTMLDivElement;
+        this.formElement = this.getFormFromTemplate();
+
+        this.titleInputElement = this.formElement.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.formElement.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.formElement.querySelector('#people')! as HTMLInputElement;
+
+        this.formElement.addEventListener('submit', this.submitHandler);
 
         this.renderHtml();
     }
 
-    private renderHtml() {
+    @Autobind
+    private submitHandler(e: Event) {
+        e.preventDefault();
+        console.log('submit');
+        console.log(this.titleInputElement.value);
+    }
+
+    private getFormFromTemplate() {
         const templateContent = document.importNode(this.templateElement.content, true);
-        const templateHtmlElement = templateContent.firstElementChild as HTMLFormElement;
-        this.appRootElement.insertAdjacentElement('afterbegin', templateHtmlElement);
+        return templateContent.firstElementChild as HTMLFormElement;
+    }
+
+    private renderHtml() {
+        this.appRootElement.insertAdjacentElement('afterbegin', this.formElement);
     }
 }
 
