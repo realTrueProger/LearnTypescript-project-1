ee6cf847b9f95853ada186c3d5f2ae279c486793
Vladimir Solovyev <vovasollo@yandex.ru>
2020-08-07 16:23:56 +0300

    add projectInputForm class

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log('test');
+class ProjectInputForm {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.appRootElement = document.getElementById('app');
+        this.renderHtml();
+    }
+    renderHtml() {
+        const templateContent = document.importNode(this.templateElement.content, true);
+        const templateHtmlElement = templateContent.firstElementChild;
+        this.appRootElement.insertAdjacentElement('afterbegin', templateHtmlElement);
+    }
+}
+const projectInput = new ProjectInputForm();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log('test');
+class ProjectInputForm {
+    templateElement: HTMLTemplateElement;
+    appRootElement: HTMLDivElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.appRootElement = document.getElementById('app')! as HTMLDivElement;
+
+        this.renderHtml();
+    }
+
+    private renderHtml() {
+        const templateContent = document.importNode(this.templateElement.content, true);
+        const templateHtmlElement = templateContent.firstElementChild as HTMLFormElement;
+        this.appRootElement.insertAdjacentElement('afterbegin', templateHtmlElement);
+    }
+}
+
+
+const projectInput = new ProjectInputForm();
